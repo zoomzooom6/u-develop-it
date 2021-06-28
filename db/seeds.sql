d24be07a63416c66ccfc7abd41056cbdf1ba9540
zoomzoom6 <tonyhuang107@live.ca>
2021-06-28 19:27:07 -0400

    initial setup of sql database files

---
+++ db/seeds.sql
@@ -0,0 +1,13 @@
+INSERT INTO candidates (first_name, last_name, industry_connected)
+VALUES
+    ('Janet', 'Monkey', 1),
+    ('Tony', 'Potato', 1),
+    ('Gumbo', 'Bumbo', 0),
+    ('Chuck', 'Barley', 1),
+    ('Dora', 'TheExplorer', 1),
+    ('Boots', 'TheMonkey', 0),
+    ('Swiper', 'TheFox', 0),
+    ('Unicorn', 'Rainbows', 1),
+    ('Rilakkuma', 'Bear', 1),
+    ('Korilakkuma', 'Bear', 1),
+    ('Banana', 'Monkey', 0);
