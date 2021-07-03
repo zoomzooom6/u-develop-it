e092cdf8fa8c6b8f7042ae065a369f19868ec14d
zoomzoom6 <tonyhuang107@live.ca>
2021-07-03 11:34:23 -0400

    connect database to server

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mysql = require("mysql2");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -6,7 +7,22 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+//Connection to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        //Your MySQL username,
+        user: 'root',
+        //Your MySQL password,
+        password: '',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 //Default response for any other request (Not Found) 
 app.use((req, res) => {
