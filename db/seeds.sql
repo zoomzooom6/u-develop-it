70e6dfe3ea71cae0322e3b8a8282b7d7fd0d7a49
zoomzoom6 <tonyhuang107@live.ca>
2021-07-03 15:59:36 -0400

    create parties table with seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -11,3 +11,9 @@ VALUES
     ('Rilakkuma', 'Bear', 1),
     ('Korilakkuma', 'Bear', 1),
     ('Banana', 'Monkey', 0);
+
+INSERT INTO parties (name, description)
+VALUES
+  ('JS Juggernauts', 'The JS Juggernauts eat, breathe, and sleep JavaScript. They can build everything you could ever want in JS, including a new kitchen sink.'),
+  ('Heroes of HTML', 'Want to see a mock-up turn into an actual webpage in a matter of minutes? Well, the Heroes of HTML can get it done in a matter of seconds.'),
+  ('Git Gurus', 'Need to resolve a merge conflict? The Git Gurus have your back. Nobody knows Git like these folks do.');
